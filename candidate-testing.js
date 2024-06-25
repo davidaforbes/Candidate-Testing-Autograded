0f7af7b65ef5a695fc8c9c76e40b20b7ec0c927b
davidaforbes <davidarthurforbes@gmail.com>
2024-06-24 21:52:29 -0400

    Not sure why I'm failing test 8?

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,9 +11,21 @@ let candidateAnswer = '';
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions =[
+  'Who was the first American woman in space?   ',
+  'True or False: 5 kilometers == 5000 meters   ',
+  '(5 + 3)/2 * 10 = ?   ',
+  'Given the array [8, "object", "trajectory", 45], what entry is at index 2?   ',
+  'What is the minimum crew size for the ISS?   ' 
+];
+let correctAnswers =[
+  'Sally Ride',
+  'true',
+  '40',
+  'Trajectory',
+  '3'
+];
+let candidateAnswers = [];
 
 
 function askForName() {
@@ -23,18 +35,19 @@ candidateName = input.question("What is your name, candidate? ");
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = input.question(question)
-
+  let i = 0
+while (i < questions.length) {
+  candidateAnswer = input.question(questions[i]);
+  candidateAnswers.push(candidateAnswer);
+  i++
 }
-
+};
 function gradeQuiz(candidateAnswers) {
-
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if (candidateAnswer == correctAnswer){
-  console.log('Correct!')
-} else {
-  console.log('Sorry, try again!')
-}
+  for (let i = 0; i < candidateAnswers.length; i++) {
+    console.log(`Your Answer: ${candidateAnswers[i]}....Correct Answer: ${correctAnswers[i]}`);
+  }
+
 
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
