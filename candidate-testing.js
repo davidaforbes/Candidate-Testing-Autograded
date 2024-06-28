5a270496fbed6fd0d76025880f84b5e2a62e15aa
davidaforbes <davidarthurforbes@gmail.com>
2024-06-27 20:15:56 -0400

    all tests passed

--- candidate-testing.js
+++ candidate-testing.js
@@ -44,16 +44,31 @@ while (i < questions.length) {
 };
 function gradeQuiz(candidateAnswers) {
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+ let grade = 0;
+
+let upperCorrect = correctAnswers.map(function (n) {
+    return n.toUpperCase();
+});
+let upperCandidate = candidateAnswers.map(function (n) {
+  return n.toUpperCase();
+});
+for (i=0; i < correctAnswers.length; i++){
+    if (upperCorrect[i] === upperCandidate[i]){
+      grade += 100 / correctAnswers.length
+    } else{}
+}
+if (grade >= 80){
+  console.log(`Congratulations ${candidateName}, you've passed with a score of ${grade}%.`)
   for (let i = 0; i < candidateAnswers.length; i++) {
     console.log(`Your Answer: ${candidateAnswers[i]}....Correct Answer: ${correctAnswers[i]}`);
   }
+} else {
+  console.log(`I'm sorry ${candidateName}, your score of ${grade}% was not high enough to pass. Please try again!`)
+}
 
-
-
-  let grade;  //TODO 3.2 use this variable to calculate the candidates score.
-
-
+    //TODO 3.2 use this variable to calculate the candidates score.
   return grade;
+  
 }
 
 function runProgram() {
@@ -62,6 +77,7 @@ function runProgram() {
   console.log(`Hello ${candidateName}, please carefully answer the following questions.`);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
+  
 }
 
 
